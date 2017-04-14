d6704db33fd274214b2a48f4f9b608494d1baa3d
Ptaha Pavel <avgustares@gmail.com>
2017-04-14 12:01:48 +0300

    add jsp portlet sample

---
+++ portlet.jsp/src/main/java/com/liferay/samples/portlet/jsp/JSPPortlet.java
@@ -0,0 +1,28 @@
+package com.liferay.samples.portlet.jsp;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=portlet-jsp",
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Blade JSP Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class JSPPortlet extends MVCPortlet {
+}
