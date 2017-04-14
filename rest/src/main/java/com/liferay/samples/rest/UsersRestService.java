f03f1b9d06c49bf8e30420f99d540e02455bf048
Ptaha Pavel <avgustares@gmail.com>
2017-04-14 12:23:46 +0300

    add rest sample

---
+++ rest/src/main/java/com/liferay/samples/rest/UsersRestService.java
@@ -0,0 +1,50 @@
+package com.liferay.samples.rest;
+
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Liferay
+ */
+@ApplicationPath("/users")
+@Component(
+	immediate = true, property = {"jaxrs.application=true"},
+	service = Application.class
+)
+public class UsersRestService extends Application {
+
+	@Override
+	public Set<Object> getSingletons() {
+		return Collections.singleton((Object)this);
+	}
+
+	@GET
+	@Path("/list")
+	@Produces("text/plain")
+	public String getUsers() {
+		StringBuilder result = new StringBuilder();
+
+		for (User user : _userLocalService.getUsers(QueryUtil.ALL_POS, -QueryUtil.ALL_POS)) {
+			result.append(user.getFullName()).append(",\n");
+		}
+
+		return result.toString();
+	}
+
+	@Reference
+	private volatile UserLocalService _userLocalService;
+
+}
